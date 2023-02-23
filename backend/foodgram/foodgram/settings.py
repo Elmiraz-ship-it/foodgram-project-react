10c468d60acdcc8926733f2db3f6dfd4aeee1ee6
Эльмира Зайнагабдинова <elmiraz1.z@yandex.ru>
2023-02-23 20:09:45 +0300

    docker-compose upd

--- backend/foodgram/foodgram/settings.py
+++ backend/foodgram/foodgram/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-2z8)g=0ski))@4c&_ew61ckgl01h)ax_sl@hp4wm511$4^f%**
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -80,22 +80,22 @@ WSGI_APPLICATION = 'foodgram.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'postgres',
-        'USER': 'postgres',
-        'PASSWORD': 'postgres',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'postgres',
+#         'USER': 'postgres',
+#         'PASSWORD': 'qwerty',
+#         'HOST': 'db',
+#         'PORT': '5432',
+#     }
+# }
 
 
 # Password validation
--- backend/foodgram/recipes/admin.py
+++ backend/foodgram/recipes/admin.py
@@ -3,7 +3,17 @@ from django.contrib import admin
 from recipes.models import Ingredient, IngredientToRecipe, Recipe, Tag
 
 
-admin.site.register(Recipe)
+class IngredientRecipeInline(admin.StackedInline):
+    model = IngredientToRecipe
+    min_num = 1
+
+
+@admin.register(Recipe)
+class RecipeAdmin(admin.ModelAdmin):
+    inlines = (IngredientRecipeInline,)
+
+
+# admin.site.register(Recipe)
 admin.site.register(Tag)
 admin.site.register(Ingredient)
 admin.site.register(IngredientToRecipe)
