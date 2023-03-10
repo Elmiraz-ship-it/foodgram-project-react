f3d27b4ab076f82701b94073656d6e3a4286fab5
Эльмира Зайнагабдинова <elmiraz1.z@yandex.ru>
2023-03-10 20:51:16 +0300

    upd settings

--- backend/foodgram/foodgram/settings.py
+++ backend/foodgram/foodgram/settings.py
@@ -162,5 +162,5 @@ DJOSER = {
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-MEDIA_ROOT = f'{BASE_DIR}/media'
 MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
