52744f7ba4ea11eee43e9b9a237ad43f716f11fe
Эльмира Зайнагабдинова <elmiraz1.z@yandex.ru>
2023-03-21 21:05:18 +0300

    upd urls

--- backend/foodgram/api/urls.py
+++ backend/foodgram/api/urls.py
@@ -14,13 +14,8 @@ urlpatterns = [
     path('tags/<int:pk>/', TagApiView.as_view()),
     path('ingredients/', IngredientApiView.as_view()),
     path('ingredients/<int:pk>/', IngredientApiView.as_view()),
-]
-
-users_urlpatterns = [
+    path('users/subscriptions/', SubscribeAPIView.as_view()),
     path('auth/', include('djoser.urls.authtoken')),
     path('', include('djoser.urls')),
-    path('subscriptions/', SubscribeAPIView.as_view()),
     path('users/<int:pk>/subscribe/', SubscribeAPIView.as_view())
 ]
-
-urlpatterns.extend(users_urlpatterns)
